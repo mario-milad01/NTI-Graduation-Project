70dee88761a1ea019b28232068a6b75119f335c2
احمد شاكر توفيق محمد كسبر stu <325250246@sha.edu.eg>
2026-07-28 02:38:55 +0300

    local progress

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { ProductDetailsPage } from './product-details-page/product-details-page';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ProductDetailsPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('E-Commerce-app');
-}
+export class AppComponent {}
---
+++ src/app/product-details-page/product-details-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details-page',
+  imports: [],
+  templateUrl: './product-details-page.html',
+  styleUrl: './product-details-page.css',
+})
+export class ProductDetailsPage {}
