a7cd350145142b378b0b1833e87cb194922458e1
Mario-Milad01 <mario.milad2000@gmail.com>
2026-07-27 21:13:06 +0300

    added navbar component and update some changes in homePage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    // {path:'login',component:LoginPage},
+    // {path:'signup',component:SignupPage}
+    {path:'home',component:HomePage},
+    // {path:'profile',component:ProfilePage},
+    // {path:'cart',component:CartPage},
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePage } from "./home-page/home-page";
+import { Navbar } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePage],
+  imports: [RouterOutlet, HomePage, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
