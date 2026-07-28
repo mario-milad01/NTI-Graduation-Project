5ae772fe1912d35451d81484958e6b1fae1a0fe7
KarimWael70 <karemwael8c@gmail.com>
2026-07-28 16:26:20 +0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home-page/home-page';
-import { CartPageComponent } from '../cart-page/cart-page';
+import { CartPageComponent } from './cart-page/cart-page';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePage } from "./home-page/home-page";
 import { Navbar } from "./navbar/navbar.component";
+import { CartPageComponent } from "./cart-page/cart-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePage, Navbar],
+  imports: [RouterOutlet, HomePage, Navbar, CartPageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
